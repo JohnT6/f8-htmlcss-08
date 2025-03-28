d25fbbdf74a1223ea240c283b75dbba256236111
John Tran <phuocthanh03062004@gmail.com>
2025-03-28 23:35:29 +0700

    Làm phần nội dung bài viết #2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -208,3 +208,25 @@ window.addEventListener("template-loaded", () => {
 // Điều chỉnh thông số chạy slide theo giây(ms)
 //     setTimeout(showSlides, 5000);
 // }
+
+window.addEventListener("template-loaded", () => {
+    const tabsSelector = "prod-tab__item";
+    const contentsSelector = "prod-tab__content";
+
+    const tabActive = `${tabsSelector}--current`;
+    const contentActive = `${contentsSelector}--current`;
+
+    const tabContainers = $$(".js-tabs");
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+                tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
+    });
+});
